935285fa8f18de6cd7cfeff51d2a71571d9c6b25
Sergey Agafonov <serj13serj@yandex.ru>
2025-07-26 09:18:25 +0300

    beautify menu

--- src/MSDOG/Assets/Scripts/UI/Menu/MenuController.cs
+++ src/MSDOG/Assets/Scripts/UI/Menu/MenuController.cs
@@ -1,5 +1,3 @@
-using Infrastructure;
-using Infrastructure.StateMachine;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,24 +6,31 @@ namespace UI.Menu
     public class MenuController : MonoBehaviour
     {
         [SerializeField] private Button _startGameButton;
+        [SerializeField] private Button _optionsButton;
         [SerializeField] private Button _quitGameButton;
         [SerializeField] private MenuLevelsWindow _menuLevelsWindow;
 
         private void OnEnable()
         {
             _startGameButton.onClick.AddListener(StartGame);
+            _optionsButton.onClick.AddListener(ShowOptions);
             _quitGameButton.onClick.AddListener(Quit);
         }
 
         private void OnDisable()
         {
             _startGameButton.onClick.RemoveListener(StartGame);
+            _optionsButton.onClick.RemoveListener(ShowOptions);
             _quitGameButton.onClick.RemoveListener(Quit);
         }
 
         private void StartGame()
         {
             _menuLevelsWindow.Show();
+        }
+
+        private void ShowOptions()
+        {
         }
 
         private void Quit()
