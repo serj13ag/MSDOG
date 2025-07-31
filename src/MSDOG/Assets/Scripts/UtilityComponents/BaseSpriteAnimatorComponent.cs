eef183a91cb777e057ce8edf90f8b8c6920e479e
Sergey Agafonov <serj13serj@yandex.ru>
2025-07-31 17:15:30 +0300

    refactor to BaseSpriteAnimatorComponent

---
+++ src/MSDOG/Assets/Scripts/UtilityComponents/BaseSpriteAnimatorComponent.cs
@@ -0,0 +1,108 @@
+using UnityEngine;
+
+namespace UtilityComponents
+{
+    public abstract class BaseSpriteAnimatorComponent : MonoBehaviour
+    {
+        [SerializeField] private Sprite[] _sprites;
+        [SerializeField] private float _timePerFrame;
+        [SerializeField] private bool _isLooped;
+        [SerializeField] private bool _destroyOnFinish;
+        [SerializeField] private bool _deactivateOnStart;
+
+        private bool _isActive;
+        private float _timeTillChangeSprite;
+        private int _frameIndex;
+
+        private void Start()
+        {
+            if (_deactivateOnStart)
+            {
+                return;
+            }
+
+            _isActive = true;
+            ResetAndUpdateSprite();
+        }
+
+        public void Activate()
+        {
+            if (_isActive)
+            {
+                return;
+            }
+
+            _isActive = true;
+            ResetAndUpdateSprite();
+        }
+
+        public void Deactivate()
+        {
+            if (!_isActive)
+            {
+                return;
+            }
+
+            _isActive = false;
+            ResetAndUpdateSprite();
+        }
+
+        private void Update()
+        {
+            if (!_isActive)
+            {
+                return;
+            }
+
+            if (_timeTillChangeSprite > 0f)
+            {
+                _timeTillChangeSprite -= Time.deltaTime;
+            }
+            else
+            {
+                if (_sprites.Length - 1 <= _frameIndex)
+                {
+                    LoopEnded();
+                }
+                else
+                {
+                    ResetTimer();
+                    _frameIndex++;
+                    ChangeSprite(_sprites[_frameIndex]);
+                }
+            }
+        }
+
+        private void LoopEnded()
+        {
+            if (_isLooped)
+            {
+                ResetAndUpdateSprite();
+            }
+            else
+            {
+                _isActive = false;
+                ChangeSprite(null);
+
+                if (_destroyOnFinish)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+        protected abstract void ChangeSprite(Sprite sprite);
+
+        private void ResetAndUpdateSprite()
+        {
+            _frameIndex = 0;
+            ChangeSprite(_sprites[_frameIndex]);
+            ResetTimer();
+        }
+
+        private void ResetTimer()
+        {
+            _timeTillChangeSprite = _timePerFrame;
+        }
+    }
+}
--- src/MSDOG/Assets/Scripts/UtilityComponents/ImageAnimatorComponent.cs
+++ src/MSDOG/Assets/Scripts/UtilityComponents/ImageAnimatorComponent.cs
@@ -3,111 +3,13 @@ using UnityEngine.UI;
 
 namespace UtilityComponents
 {
-    public class ImageAnimatorComponent : MonoBehaviour
+    public class ImageAnimatorComponent : BaseSpriteAnimatorComponent
     {
         [SerializeField] private Image _image;
-        [SerializeField] private Sprite[] _sprites;
-        [SerializeField] private float _timePerFrame;
-        [SerializeField] private bool _isLooped;
-        [SerializeField] private bool _destroyOnFinish;
-        [SerializeField] private bool _deactivateOnStart;
 
-        private bool _isActive;
-        private float _timeTillChangeSprite;
-        private int _frameIndex;
-
-        private void Start()
-        {
-            if (_deactivateOnStart)
-            {
-                return;
-            }
-
-            _isActive = true;
-            ResetAndUpdateSprite();
-        }
-
-        public void Activate()
-        {
-            if (_isActive)
-            {
-                return;
-            }
-
-            _isActive = true;
-            ResetAndUpdateSprite();
-        }
-
-        public void Deactivate()
-        {
-            if (!_isActive)
-            {
-                return;
-            }
-
-            _isActive = false;
-            ResetAndUpdateSprite();
-        }
-
-        private void Update()
-        {
-            if (!_isActive)
-            {
-                return;
-            }
-
-            if (_timeTillChangeSprite > 0f)
-            {
-                _timeTillChangeSprite -= Time.deltaTime;
-            }
-            else
-            {
-                if (_sprites.Length - 1 <= _frameIndex)
-                {
-                    LoopEnded();
-                }
-                else
-                {
-                    ResetTimer();
-                    _frameIndex++;
-                    ChangeSprite(_sprites[_frameIndex]);
-                }
-            }
-        }
-
-        private void LoopEnded()
-        {
-            if (_isLooped)
-            {
-                ResetAndUpdateSprite();
-            }
-            else
-            {
-                _isActive = false;
-                ChangeSprite(null);
-
-                if (_destroyOnFinish)
-                {
-                    Destroy(gameObject);
-                }
-            }
-        }
-
-        private void ChangeSprite(Sprite sprite)
+        protected override void ChangeSprite(Sprite sprite)
         {
             _image.sprite = sprite;
-        }
-
-        private void ResetAndUpdateSprite()
-        {
-            _frameIndex = 0;
-            ChangeSprite(_sprites[_frameIndex]);
-            ResetTimer();
-        }
-
-        private void ResetTimer()
-        {
-            _timeTillChangeSprite = _timePerFrame;
         }
     }
 }
