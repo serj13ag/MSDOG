b78e2ddc8e38b855035eb2c368529861bfde1be8
Sergey Agafonov <serj13serj@yandex.ru>
2025-08-19 21:01:59 +0300

    move init logic to GameplayTvHudInitializer

--- src/MSDOG/Assets/Scripts/Infrastructure/GameplayTvHudInitializer.cs
+++ src/MSDOG/Assets/Scripts/Infrastructure/GameplayTvHudInitializer.cs
@@ -1,31 +1,43 @@
+using Core.Services;
 using Gameplay.Controllers;
 using Gameplay.Providers;
+using Gameplay.Services;
 using UI.HUD;
 using UI.HUD.Actions;
+using UI.HUD.DetailsZone;
 
 namespace Infrastructure
 {
     public class GameplayTvHudInitializer
     {
+        private readonly IDataService _dataService;
+        private readonly ILevelFlowService _levelFlowService;
         private readonly HudController _hudController;
         private readonly HealthBarHud _healthBarHud;
         private readonly ExperienceBarHud _experienceBarHud;
+        private readonly ActiveZoneHud _activeZoneHud;
         private readonly HudActions _hudActions;
         private readonly IEscapeWindowHandler _escapeWindowHandler;
         private readonly IPlayerProvider _playerProvider;
         private readonly IDebugController _debugController;
 
-        public GameplayTvHudInitializer(HudController hudController,
+        public GameplayTvHudInitializer(IDataService dataService,
+            ILevelFlowService levelFlowService,
+            HudController hudController,
             HealthBarHud healthBarHud,
             ExperienceBarHud experienceBarHud,
+            ActiveZoneHud activeZoneHud,
             HudActions hudActions,
             IEscapeWindowHandler escapeWindowHandler,
             IPlayerProvider playerProvider,
             IDebugController debugController)
         {
+            _dataService = dataService;
+            _levelFlowService = levelFlowService;
             _hudController = hudController;
             _healthBarHud = healthBarHud;
             _experienceBarHud = experienceBarHud;
+            _activeZoneHud = activeZoneHud;
             _hudActions = hudActions;
             _escapeWindowHandler = escapeWindowHandler;
             _playerProvider = playerProvider;
@@ -39,8 +51,11 @@ namespace Infrastructure
 
             _escapeWindowHandler.Init();
 
-            // TODO: refactor if needed
-            _hudController.AddStartAbility();
+            var startAbilitiesData = _dataService.GetStartAbilitiesData(_levelFlowService.CurrentLevelIndex);
+            foreach (var abilityData in startAbilitiesData)
+            {
+                _activeZoneHud.AddDetail(abilityData);
+            }
 
             _hudActions.Init(_playerProvider.Player);
 
--- src/MSDOG/Assets/Scripts/UI/HUD/HudController.cs
+++ src/MSDOG/Assets/Scripts/UI/HUD/HudController.cs
@@ -1,6 +1,4 @@
 using Core.Models.Data;
-using Core.Services;
-using Gameplay.Services;
 using UI.HUD.DetailsZone;
 using UnityEngine;
 using VContainer;
@@ -11,30 +9,12 @@ namespace UI.HUD
     {
         [SerializeField] private Transform _canvasTransform;
 
-        private IDataService _dataService;
-        private ILevelFlowService _levelFlowService;
-
         private DetailsZoneHud _detailsZoneHud;
-        private ActiveZoneHud _activeZoneHud;
 
         [Inject]
-        public void Construct(IDataService dataService, ILevelFlowService levelFlowService, DetailsZoneHud detailsZoneHud,
-            ActiveZoneHud activeZoneHud)
+        public void Construct(DetailsZoneHud detailsZoneHud)
         {
-            _levelFlowService = levelFlowService;
-            _dataService = dataService;
-
             _detailsZoneHud = detailsZoneHud;
-            _activeZoneHud = activeZoneHud;
-        }
-
-        public void AddStartAbility()
-        {
-            var startAbilitiesData = _dataService.GetStartAbilitiesData(_levelFlowService.CurrentLevelIndex);
-            foreach (var abilityData in startAbilitiesData)
-            {
-                _activeZoneHud.AddDetail(abilityData);
-            }
         }
 
         public void AddAbility(AbilityData abilityData)
